49b85c0f3a46bcac6447afdcd2f24c4d489e378f
ikhsanova22 <43130716+ikhsanova22@users.noreply.github.com>
2018-12-01 00:01:51 +0500

    Update task12C.php

--- week12/task12C.php
+++ week12/task12C.php
@@ -2,15 +2,15 @@
 <?php
     session_start();
     $dbhost = 'localhost';
-    $dbuser = 'root';
-    $dbpass = '';
+    $username = 'root';
+    $password = '';
     $db = 'webtasks';
-    $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $db);
+    $conn = mysqli_connect($dbhost, $username, $password, $db);
     if (isset($_POST['name'])) {
         $login = $_POST['name'];
-        $pass = $_POST['pass'];
-        $sql = "SELECT * FROM admins WHERE username='$login' AND pass='$pass'";
-        $result = $conn->query($sql);
+        $password = $_POST['pass'];
+        $query = "SELECT * FROM admins WHERE username='$login' AND pass='$password'";
+        $result = $conn->query($query);
         if ($result->num_rows == 1) {
             $row = mysqli_fetch_assoc($result);
             $_SESSION['login'] = $login;
